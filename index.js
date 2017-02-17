7dc6c91d0b0a3f7e91ce857f934f2ab907fc3a2d
Tayeb Mc <gomail654@gmail.com>
2017-02-17 18:41:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+  array.forEach(element, index)=>{
+  console.log(index + ": " + element);
+});
+}
+
+function iterate(callback){
+  var friends = ["John", "Doris", "Lina"];
+      friends.forEach(callback)
+        return friends
+  }
+
+  function doToArray(array, callback){
+    array.forEach(callback)
+  }
